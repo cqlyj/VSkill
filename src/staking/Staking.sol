4a7e536cd8d2e1cc2d79bc63e32ab6001493c005
cqlyj <luoyingjie0721@gmail.com>
2024-07-14 23:44:43 +0800

    chore: Add Staking contract and tests for staking functionality

---
+++ src/staking/Staking.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+pragma solidity ^0.8.24;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceCoverter.sol";
+
+error Staking__NotEnoughMoneyStaked();
+error Staking__NotEnoughBalanceToWithdraw();
+error Staking__WithdrawFailed();
+
+contract Staking {
+    using PriceConverter for uint256;
+
+    uint256 private constant MIN_USD_AMOUNT = 20e18; // 20 USD
+    mapping(address => uint256) private addressToMoneyStaked;
+    address[] private verifiers;
+    AggregatorV3Interface internal priceFeed;
+
+    event Staked(address indexed staker, uint256 amount);
+
+    constructor(address _priceFeed) {
+        priceFeed = AggregatorV3Interface(_priceFeed);
+    }
+
+    receive() external payable {
+        stakeToBeTheVerifier();
+    }
+
+    fallback() external payable {
+        stakeToBeTheVerifier();
+    }
+
+    function withdrawStake(uint256 amountToWithdraw) external {
+        if (addressToMoneyStaked[msg.sender] < amountToWithdraw) {
+            revert Staking__NotEnoughBalanceToWithdraw();
+        }
+        addressToMoneyStaked[msg.sender] -= amountToWithdraw;
+        (bool success, ) = msg.sender.call{value: amountToWithdraw}("");
+        if (!success) {
+            revert Staking__WithdrawFailed();
+        }
+    }
+
+    function stakeToBeTheVerifier() public payable {
+        if (msg.value.convertEthToUsd(priceFeed) < MIN_USD_AMOUNT) {
+            revert Staking__NotEnoughMoneyStaked();
+        }
+        addressToMoneyStaked[msg.sender] += msg.value;
+        verifiers.push(msg.sender);
+        emit Staked(msg.sender, msg.value);
+    }
+
+    ///////////////////////////////
+    /////   Getter Functions   ////
+    ///////////////////////////////
+
+    function getMinUsdAmount() external pure returns (uint256) {
+        return MIN_USD_AMOUNT;
+    }
+
+    function getMoneyStaked(address staker) external view returns (uint256) {
+        return addressToMoneyStaked[staker];
+    }
+
+    function getVerifiers(uint256 index) external view returns (address) {
+        return verifiers[index];
+    }
+
+    function getVerifiersLength() external view returns (uint256) {
+        return verifiers.length;
+    }
+}
--- test/staking/StakingTest.t.sol
+++ test/staking/StakingTest.t.sol
@@ -8,14 +8,57 @@ import {DeployStaking} from "script/staking/DeployStaking.s.sol";
 
 contract StakingTest is Test {
     Staking staking;
+    uint256 public constant MIN_USD_AMOUNT = 20e18;
+    uint256 public constant INITIAL_BALANCE = 100e18;
+    address public USER = makeAddr("user");
 
     function setUp() external {
         DeployStaking deployer = new DeployStaking();
         staking = deployer.run();
+        vm.deal(USER, INITIAL_BALANCE);
     }
 
     function testMinUsdAmountIsTwenty() external view {
         uint256 minUsdAmount = staking.getMinUsdAmount();
         assertEq(minUsdAmount, 20e18);
+    }
+
+    function testStakeRevertIfNotEnoughUsd() external {
+        vm.expectRevert();
+        staking.stakeToBeTheVerifier();
+    }
+
+    function testStakeOnlyIfEnoughUsd() external {
+        vm.startPrank(USER);
+        staking.stakeToBeTheVerifier{value: MIN_USD_AMOUNT}();
+        vm.stopPrank();
+
+        uint256 balance = staking.getMoneyStaked(USER);
+        assertEq(balance, MIN_USD_AMOUNT);
+    }
+
+    function testStakeSuccessUpdatesVerifier() external {
+        vm.startPrank(USER);
+        staking.stakeToBeTheVerifier{value: MIN_USD_AMOUNT}();
+        vm.stopPrank();
+
+        address verifiers = staking.getVerifiers(0);
+        assertEq(verifiers, USER);
+        assertEq(staking.getVerifiersLength(), 1);
+    }
+
+    function testWithdrawRevertIfNotEnoughBalance() external {
+        vm.expectRevert();
+        staking.withdrawStake(MIN_USD_AMOUNT);
+    }
+
+    function testWithdrawSuccessUpdatesBalance() external {
+        vm.startPrank(USER);
+        staking.stakeToBeTheVerifier{value: MIN_USD_AMOUNT}();
+        staking.withdrawStake(MIN_USD_AMOUNT / 2);
+        vm.stopPrank();
+
+        uint256 balance = staking.getMoneyStaked(USER);
+        assertEq(balance, MIN_USD_AMOUNT / 2);
     }
 }
