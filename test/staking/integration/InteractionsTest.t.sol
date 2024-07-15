dabd9b8ce75e1ad090995e2d7d8f38415395f2bc
cqlyj <luoyingjie0721@gmail.com>
2024-07-15 15:31:12 +0800

    chore: Add InteractionsTest for testing staking interactions

---
+++ test/staking/integration/InteractionsTest.t.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Test, console} from "lib/forge-std/src/Test.sol";
+import {Staking} from "src/staking/Staking.sol";
+import {DeployStaking} from "script/staking/DeployStaking.s.sol";
+import {StakeToBeTheVerifierStaking, WithdrawStakeStaking, StakeStaking} from "script/staking/Interactions.s.sol";
+
+contract InteractionsTest is Test {
+    Staking staking;
+
+    function setUp() external {
+        DeployStaking deployer = new DeployStaking();
+        staking = deployer.run();
+    }
+
+    function testInteractions() external {
+        StakeToBeTheVerifierStaking stakerVerifier = new StakeToBeTheVerifierStaking();
+        stakerVerifier.stakeToBeTheVerifierStaking(address(staking));
+
+        assertEq(staking.getVerifierCount(), 1);
+        console.log("Stake to be the verifier success");
+
+        WithdrawStakeStaking withdrawer = new WithdrawStakeStaking();
+        withdrawer.withdrawStakeStaking(address(staking));
+
+        assertEq(staking.getVerifierCount(), 0);
+        console.log("Withdraw stake success");
+
+        StakeStaking staker = new StakeStaking();
+        staker.stakeStaking(address(staking));
+
+        assertEq(staking.getVerifierCount(), 1);
+        console.log("Stake success");
+    }
+}
