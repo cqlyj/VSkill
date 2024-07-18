4a9335e857d1dfec97c1743c9dc944ed3ff135bf
cqlyj <luoyingjie0721@gmail.com>
2024-07-18 18:05:18 +0800

    chore: Update submodule URLs and add Distribution.sol contract
    ```

---
+++ src/oracle/Distribution.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+pragma solidity ^0.8.24;
+
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+
+contract Distribution is VRFConsumerBaseV2 {
+    uint64 subscriptionId;
+    VRFCoordinatorV2Interface vrfCoordinator;
+    bytes32 keyHash;
+    uint32 callbackGasLimit;
+    uint16 requestConfirmations;
+    uint32 numWords;
+
+    uint256[] private randomWords;
+
+    // mapping(string => address[]) verifiersWithinSameDomain;
+    // First get the address of those verifiers who are within the same domain
+    // Then based on the random number generated, select the verifier
+    // Those verifiers have the reputation score, higher reputation scores means higher probability of being selected
+    // Come back when write the contract for verifiers
+
+    constructor(
+        uint64 _subscriptionId,
+        address _vrfCoordinator,
+        bytes32 _keyHash,
+        uint32 _callbackGasLimit,
+        uint16 _requestConfirmations,
+        uint32 _numWords
+    ) VRFConsumerBaseV2(_vrfCoordinator) {
+        subscriptionId = _subscriptionId;
+        vrfCoordinator = VRFCoordinatorV2Interface(_vrfCoordinator);
+        keyHash = _keyHash;
+        callbackGasLimit = _callbackGasLimit;
+        requestConfirmations = _requestConfirmations;
+        numWords = _numWords;
+    }
+
+    function distributionRandomNumberForVerifiers()
+        external
+        returns (uint256 requestId)
+    {
+        requestId = vrfCoordinator.requestRandomWords(
+            keyHash,
+            subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+        return requestId;
+    }
+
+    function fulfillRandomWords(
+        uint256 /*_requestId*/,
+        uint256[] memory _randomWords
+    ) internal override {
+        randomWords = _randomWords;
+    }
+
+    ///////////////////////////////
+    /////   Getter Functions   ////
+    ///////////////////////////////
+
+    function getRandomWords() external view returns (uint256[] memory) {
+        return randomWords;
+    }
+}
