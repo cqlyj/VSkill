8a07837f4e95eeb0576da424b9b7e838f66add51
cqlyj <luoyingjie0721@gmail.com>
2024-07-18 23:37:42 +0800

    chore: Update DeployDistribution script to include subscription creation, funding, and consumer addition

--- script/oracle/DeployDistribution.s.sol
+++ script/oracle/DeployDistribution.s.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.24;
 import {Script} from "forge-std/Script.sol";
 import {Distribution} from "../../src/oracle/Distribution.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
+import {CreateSubscriptionDistribution, FundSubscriptionDistribution, AddConsumerDistribution} from "./Interactions.s.sol";
 
 contract DeployDistribution is Script {
     function run() external returns (Distribution, HelperConfig) {
@@ -14,20 +15,45 @@ contract DeployDistribution is Script {
             address vrfCoordinator,
             bytes32 keyHash,
             uint32 callbackGasLimit,
-            uint16 requestConfirmations,
-            uint32 numWords
+            address linkTokenAddress,
+            uint256 deployerKey
         ) = helperConfig.activeNetworkConfig();
+
+        if (subscriptionId == 0) {
+            // Create subscription
+            CreateSubscriptionDistribution subscription = new CreateSubscriptionDistribution();
+            subscriptionId = subscription.createSubscription(
+                vrfCoordinator,
+                deployerKey
+            );
+
+            // Fund subscription
+            FundSubscriptionDistribution fund = new FundSubscriptionDistribution();
+            fund.fundSubscription(
+                vrfCoordinator,
+                subscriptionId,
+                linkTokenAddress,
+                deployerKey
+            );
+        }
 
         vm.startBroadcast();
         Distribution distribution = new Distribution(
             subscriptionId,
             vrfCoordinator,
             keyHash,
-            callbackGasLimit,
-            requestConfirmations,
-            numWords
+            callbackGasLimit
         );
         vm.stopBroadcast();
+
+        // Add consumer
+        AddConsumerDistribution consumer = new AddConsumerDistribution();
+        consumer.addConsumer(
+            address(distribution),
+            vrfCoordinator,
+            subscriptionId,
+            deployerKey
+        );
 
         return (distribution, helperConfig);
     }
