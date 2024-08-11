1de7a8644841e5f9e15a729a522b95bfb57b087f
cqlyj <luoyingjie0721@gmail.com>
2024-08-11 22:21:37 +0800

    refactor the Staking module for integration

--- src/staking/Staking.sol
+++ src/staking/Staking.sol
@@ -30,21 +30,35 @@ import {PriceConverter} from "../utils/PriceCoverter.sol";
 contract Staking {
     using PriceConverter for uint256;
 
-    error Staking__NotEnoughMoneyStaked(
-        uint256 minUsdAmount,
-        uint256 currentStakeUsdAmount
-    );
     error Staking__NotEnoughBalanceToWithdraw(uint256 currentStakeEthAmount);
+    error Staking__NotEnoughStakeToBecomeVerifier(
+        uint256 currentStakeUsdAmount,
+        uint256 minStakeUsdAmount
+    );
     error Staking__WithdrawFailed();
     error Staking__NotVerifier();
     error Staking__AlreadyVerifier();
 
+    struct verifier {
+        uint256 id;
+        address verifierAddress;
+        uint256 reputation;
+        string[] skillDomains;
+        uint256 moneyStakedInUsd;
+        string[] evidenceIpfsHash;
+        string[] feedbackIpfsHash;
+    }
+
     uint256 private constant MIN_USD_AMOUNT = 20e18; // 20 USD
+    uint256 private constant INITIAL_REPUTATION = 2;
+    uint256 private immutable LOWEST_REPUTATION = 0;
+    uint256 private immutable HIGHEST_REPUTATION = 10;
     uint256 private id; // If id is 0, then the address is not a verifier
     uint256 private verifierCount;
-    mapping(address => uint256) private addressToMoneyStaked;
-    mapping(address => uint256) private verifierToId;
+
     AggregatorV3Interface internal priceFeed;
+    mapping(address => uint256) internal addressToId;
+    verifier[] internal verifiers;
 
     event Staked(address indexed staker, uint256 amount);
     event Withdrawn(address indexed staker, uint256 amount);
@@ -57,71 +71,102 @@ contract Staking {
         verifierCount = 0;
     }
 
-    receive() external payable {
-        stakeToBeTheVerifier();
-    }
+    receive() external payable {}
 
-    fallback() external payable {
-        stakeToBeTheVerifier();
-    }
+    fallback() external payable {}
 
     function withdrawStake(uint256 amountToWithdraw) external {
-        if (addressToMoneyStaked[msg.sender] < amountToWithdraw) {
+        if (addressToId[msg.sender] == 0) {
+            revert Staking__NotVerifier();
+        }
+
+        if (
+            verifiers[addressToId[msg.sender] - 1].moneyStakedInUsd <
+            amountToWithdraw
+        ) {
             revert Staking__NotEnoughBalanceToWithdraw(
-                addressToMoneyStaked[msg.sender]
+                verifiers[addressToId[msg.sender] - 1].moneyStakedInUsd
             );
         }
-        addressToMoneyStaked[msg.sender] -= amountToWithdraw;
+
         (bool success, ) = msg.sender.call{value: amountToWithdraw}("");
         if (!success) {
             revert Staking__WithdrawFailed();
         }
+
+        verifiers[addressToId[msg.sender] - 1]
+            .moneyStakedInUsd -= amountToWithdraw;
         emit Withdrawn(msg.sender, amountToWithdraw);
 
         if (
-            addressToMoneyStaked[msg.sender].convertEthToUsd(priceFeed) <
-            MIN_USD_AMOUNT
+            !_currentStakedAmountIsStillAboveMinUsdAmount(
+                verifiers[addressToId[msg.sender] - 1].moneyStakedInUsd
+            )
         ) {
-            verifierToId[msg.sender] = 0;
+            addressToId[msg.sender] = 0;
             verifierCount--;
             emit LoseVerifier(msg.sender);
+
+            // Remove the verifier from the array
+            uint256 index = addressToId[msg.sender] - 1;
+            verifiers[index] = verifiers[verifierCount];
+            verifiers.pop();
         }
     }
 
-    // In case get penalty, stake more
-    function stake() external payable {
-        addressToMoneyStaked[msg.sender] += msg.value;
-        emit Staked(msg.sender, msg.value);
+    function stake(uint256 amountInUsd) external payable {
+        if (addressToId[msg.sender] == 0) {
+            if (amountInUsd < MIN_USD_AMOUNT) {
+                revert Staking__NotEnoughStakeToBecomeVerifier(
+                    amountInUsd,
+                    MIN_USD_AMOUNT
+                );
+            } else {
+                addressToId[msg.sender] = id;
+                verifierCount++;
+                verifiers.push(
+                    _initializeVerifier(msg.sender, new string[](0))
+                );
+                emit BecomeVerifier(id, msg.sender);
+                id++;
 
-        if (
-            addressToMoneyStaked[msg.sender].convertEthToUsd(priceFeed) >=
-            MIN_USD_AMOUNT &&
-            verifierToId[msg.sender] == 0
-        ) {
-            verifierToId[msg.sender] = id;
-            id++;
-            verifierCount++;
-            emit BecomeVerifier(verifierToId[msg.sender], msg.sender);
+                verifiers[addressToId[msg.sender] - 1].moneyStakedInUsd += msg
+                    .value
+                    .convertEthToUsd(priceFeed);
+                emit Staked(msg.sender, msg.value);
+            }
         }
+
+        verifiers[addressToId[msg.sender] - 1].moneyStakedInUsd += msg
+            .value
+            .convertEthToUsd(priceFeed);
+        emit Staked(msg.sender, msg.value);
     }
 
-    function stakeToBeTheVerifier() public payable {
-        if (msg.value.convertEthToUsd(priceFeed) < MIN_USD_AMOUNT) {
-            revert Staking__NotEnoughMoneyStaked(
-                MIN_USD_AMOUNT,
-                msg.value.convertEthToUsd(priceFeed)
-            );
-        }
-        if (verifierToId[msg.sender] != 0) {
-            revert Staking__AlreadyVerifier();
-        }
+    /////////////////////////////////
+    /////   Internal Functions   ////
+    /////////////////////////////////
 
-        addressToMoneyStaked[msg.sender] += msg.value;
-        verifierToId[msg.sender] = id;
-        id++;
-        verifierCount++;
-        emit Staked(msg.sender, msg.value);
-        emit BecomeVerifier(verifierToId[msg.sender], msg.sender);
+    function _initializeVerifier(
+        address verifierAddress,
+        string[] memory skillDomains
+    ) internal view returns (verifier memory) {
+        return
+            verifier({
+                id: id,
+                verifierAddress: verifierAddress,
+                reputation: INITIAL_REPUTATION,
+                skillDomains: skillDomains,
+                moneyStakedInUsd: 0,
+                evidenceIpfsHash: new string[](0),
+                feedbackIpfsHash: new string[](0)
+            });
+    }
+
+    function _currentStakedAmountIsStillAboveMinUsdAmount(
+        uint256 currentStakedAmount
+    ) internal pure returns (bool) {
+        return currentStakedAmount >= MIN_USD_AMOUNT;
     }
 
     ///////////////////////////////
@@ -132,19 +177,71 @@ contract Staking {
         return MIN_USD_AMOUNT;
     }
 
-    function getMoneyStaked(address staker) external view returns (uint256) {
-        return addressToMoneyStaked[staker];
-    }
-
-    function getVerifierId(address verifier) public view returns (uint256) {
-        return verifierToId[verifier];
-    }
-
     function getLatestId() external view returns (uint256) {
         return id;
     }
 
     function getVerifierCount() external view returns (uint256) {
         return verifierCount;
+    }
+
+    function getVerifierId(
+        address verifierAddress
+    ) external view returns (uint256) {
+        return addressToId[verifierAddress];
+    }
+
+    function getVerifierReputation(
+        address verifierAddress
+    ) external view returns (uint256) {
+        return verifiers[addressToId[verifierAddress] - 1].reputation;
+    }
+
+    function getVerifierSkillDomains(
+        address verifierAddress
+    ) external view returns (string[] memory) {
+        return verifiers[addressToId[verifierAddress] - 1].skillDomains;
+    }
+
+    function getVerifierMoneyStakedInUsd(
+        address verifierAddress
+    ) external view returns (uint256) {
+        return verifiers[addressToId[verifierAddress] - 1].moneyStakedInUsd;
+    }
+
+    function getVerifierEvidenceIpfsHash(
+        address verifierAddress
+    ) external view returns (string[] memory) {
+        return verifiers[addressToId[verifierAddress] - 1].evidenceIpfsHash;
+    }
+
+    function getVerifierFeedbackIpfsHash(
+        address verifierAddress
+    ) external view returns (string[] memory) {
+        return verifiers[addressToId[verifierAddress] - 1].feedbackIpfsHash;
+    }
+
+    function getInitialReputation() external pure returns (uint256) {
+        return INITIAL_REPUTATION;
+    }
+
+    function getLowestReputation() external pure returns (uint256) {
+        return LOWEST_REPUTATION;
+    }
+
+    function getHighestReputation() external pure returns (uint256) {
+        return HIGHEST_REPUTATION;
+    }
+
+    function getVerifier(
+        address verifierAddress
+    ) external view returns (verifier memory) {
+        return verifiers[addressToId[verifierAddress] - 1];
+    }
+
+    function getVerifierById(
+        uint256 _id
+    ) external view returns (verifier memory) {
+        return verifiers[_id - 1];
     }
 }
