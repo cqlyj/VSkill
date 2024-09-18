b15dd78fc65fe231f1ec4c7eed6d7753793dfa03
cqlyj <luoyingjie0721@gmail.com>
2024-09-18 14:41:06 +0800

    Refactor stakeStaking function to use ENOUGH_USD_AMOUNT constant

--- script/staking/Interactions.s.sol
+++ script/staking/Interactions.s.sol
@@ -6,26 +6,6 @@ import {Script, console} from "forge-std/Script.sol";
 import {Staking} from "src/staking/Staking.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
-contract StakeToBeTheVerifierStaking is Script {
-    uint256 public constant MIN_ETH_AMOUNT = 0.01 ether;
-
-    function stakeToBeTheVerifierStaking(address mostRecentlyDeployed) public {
-        vm.startBroadcast();
-        Staking staking = Staking(payable(mostRecentlyDeployed));
-        staking.stakeToBeTheVerifier{value: MIN_ETH_AMOUNT}();
-        vm.stopBroadcast();
-    }
-
-    function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
-            "Staking",
-            block.chainid
-        );
-        console.log("Most recently deployed address: ", mostRecentlyDeployed);
-        stakeToBeTheVerifierStaking(mostRecentlyDeployed);
-    }
-}
-
 contract WithdrawStakeStaking is Script {
     uint256 public MIN_ETH_AMOUNT = 0.01 ether;
 
@@ -47,12 +27,12 @@ contract WithdrawStakeStaking is Script {
 }
 
 contract StakeStaking is Script {
-    uint256 public constant MIN_ETH_AMOUNT = 0.01 ether;
+    uint256 public constant ENOUGH_USD_AMOUNT = 1000;
 
     function stakeStaking(address mostRecentlyDeployed) public {
         vm.startBroadcast();
         Staking staking = Staking(payable(mostRecentlyDeployed));
-        staking.stake{value: MIN_ETH_AMOUNT}();
+        staking.stake(ENOUGH_USD_AMOUNT);
         vm.stopBroadcast();
     }
 
