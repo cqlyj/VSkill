bd4f10bb56653e75c7c2d6869d80d3d457bcdae6
cqlyj <luoyingjie0721@gmail.com>
2024-10-09 19:55:10 +0800

    Refactor VerifierTest contract to improve code readability and add new test cases for the constructor, checkUpkeep, updateSkillDomains, and provideFeedback functions

--- test/verifier/unit/VerifierTest.t.sol
+++ test/verifier/unit/VerifierTest.t.sol
@@ -29,11 +29,13 @@ contract VerifierTest is Test {
     address public USER = makeAddr("user");
     uint256 private constant MIN_USD_AMOUNT_TO_STAKE = 20e18; // 20 USD
     string[] private NEW_SKILL_DOMAINS = ["newSkillDomain1", "newSkillDomain2"];
-    string[] private SKILL_DOMAINS = ["skillDomain1", "skillDomain2"];
+    string[] private SKILL_DOMAINS = ["Frontend", "Backend"];
     uint256 private constant INITIAL_BALANCE = 100 ether;
     uint256 private constant NUM_WORDS = 3;
     string public constant IPFS_HASH =
         "https://ipfs.io/ipfs/QmbJLndDmDiwdotu3MtfcjC2hC5tXeAR9EXbNSdUDUDYWa";
+    string public constant FEEDBACK_IPFS_HASH =
+        "https://ipfs.io/ipfs/QmSsYRx3LpDAb1GZQm7zZ1AuHZjfbPkD6J7s9r41xu1mf8";
 
     enum SubmissionStatus {
         SUBMITTED,
@@ -80,6 +82,9 @@ contract VerifierTest is Test {
             .getActiveNetworkConfig()
             .linkTokenAddress;
         deployerKey = helperConfig.getActiveNetworkConfig().deployerKey;
+        verifierConstructorParams.userNftImageUris = helperConfig
+            .getActiveNetworkConfig()
+            .userNftImageUris;
 
         vm.deal(USER, INITIAL_BALANCE);
     }
@@ -143,6 +148,144 @@ contract VerifierTest is Test {
         _;
     }
 
+    ///////////////////////////
+    ///     constructor     ///
+    ///////////////////////////
+
+    function testVerifierConstructorSetCorrectly() external view {
+        assertEq(
+            address(verifier.getPriceFeed()),
+            address(verifierConstructorParams.priceFeed)
+        );
+        // this subscriptionId will be updated later in the helperconfig, hence commented out
+        // assertEq(
+        //     verifier.getSubscriptionId(),
+        //     verifierConstructorParams.subscriptionId
+        // );
+        assertEq(
+            address(verifier.getVrfCoordinator()),
+            address(verifierConstructorParams.vrfCoordinator)
+        );
+        assertEq(verifier.getKeyHash(), verifierConstructorParams.keyHash);
+        assertEq(
+            verifier.getCallbackGasLimit(),
+            verifierConstructorParams.callbackGasLimit
+        );
+        assertEq(
+            verifier.getSubmissionFeeInUsd(),
+            verifierConstructorParams.submissionFeeInUsd
+        );
+        assertEq(
+            verifier.getUserNftImageUris().length,
+            verifierConstructorParams.userNftImageUris.length
+        );
+
+        assert(
+            keccak256(abi.encode(verifier.getUserNftImageUris())) ==
+                keccak256(
+                    abi.encode(verifierConstructorParams.userNftImageUris)
+                )
+        );
+    }
+
+    ///////////////////////////
+    ///     checkUpkeep     ///
+    ///////////////////////////
+
+    function testCheckUpKeepReturnsTrueIfEvidenceStatusIsSubmittedOrDifferentOpinion()
+        external
+    {
+        StructDefinition.VSkillUserEvidence memory ev1 = StructDefinition
+            .VSkillUserEvidence(
+                USER,
+                IPFS_HASH,
+                SKILL_DOMAINS[0],
+                StructDefinition.VSkillUserSubmissionStatus.SUBMITTED,
+                new string[](0)
+            );
+
+        StructDefinition.VSkillUserEvidence memory ev2 = StructDefinition
+            .VSkillUserEvidence(
+                USER,
+                IPFS_HASH,
+                SKILL_DOMAINS[0],
+                StructDefinition.VSkillUserSubmissionStatus.DIFFERENTOPINION,
+                new string[](0)
+            );
+        vm.prank(USER);
+        verifier.submitEvidence{
+            value: verifier.getSubmissionFeeInUsd().convertUsdToEth(
+                AggregatorV3Interface(verifierConstructorParams.priceFeed)
+            )
+        }(ev1.evidenceIpfsHash, ev1.skillDomain);
+        (bool upkeepNeeded1, ) = verifier.checkUpkeep("");
+        assert(upkeepNeeded1);
+
+        vm.prank(USER);
+        verifier.submitEvidence{
+            value: verifier.getSubmissionFeeInUsd().convertUsdToEth(
+                AggregatorV3Interface(verifierConstructorParams.priceFeed)
+            )
+        }(ev2.evidenceIpfsHash, ev2.skillDomain);
+        (bool upkeepNeeded2, ) = verifier.checkUpkeep("");
+        assert(upkeepNeeded2);
+    }
+
+    function testCheckUpKeepReturnsCorrectEvidenceIfEvidenceStatusIsSubmittedOrDifferentOpinion()
+        external
+    {
+        StructDefinition.VSkillUserEvidence memory ev1 = StructDefinition
+            .VSkillUserEvidence(
+                USER,
+                IPFS_HASH,
+                SKILL_DOMAINS[0],
+                StructDefinition.VSkillUserSubmissionStatus.SUBMITTED,
+                new string[](0)
+            );
+
+        StructDefinition.VSkillUserEvidence memory ev2 = StructDefinition
+            .VSkillUserEvidence(
+                USER,
+                IPFS_HASH,
+                SKILL_DOMAINS[0],
+                StructDefinition.VSkillUserSubmissionStatus.DIFFERENTOPINION,
+                new string[](0)
+            );
+        vm.prank(USER);
+        verifier.submitEvidence{
+            value: verifier.getSubmissionFeeInUsd().convertUsdToEth(
+                AggregatorV3Interface(verifierConstructorParams.priceFeed)
+            )
+        }(ev1.evidenceIpfsHash, ev1.skillDomain);
+        (, bytes memory evidence1) = verifier.checkUpkeep("");
+        assertEq(
+            abi
+                .decode(evidence1, (StructDefinition.VSkillUserEvidence))
+                .evidenceIpfsHash,
+            ev1.evidenceIpfsHash
+        );
+
+        vm.prank(USER);
+        verifier.submitEvidence{
+            value: verifier.getSubmissionFeeInUsd().convertUsdToEth(
+                AggregatorV3Interface(verifierConstructorParams.priceFeed)
+            )
+        }(ev2.evidenceIpfsHash, ev2.skillDomain);
+        (, bytes memory evidence2) = verifier.checkUpkeep("");
+        assertEq(
+            abi
+                .decode(evidence2, (StructDefinition.VSkillUserEvidence))
+                .evidenceIpfsHash,
+            ev2.evidenceIpfsHash
+        );
+    }
+
+    // since we cannot change the status of the evidence unless we have those verifiers to provide feedback
+    // this test case will be skipped
+    // function testCheckUpKeepReturnsFalseIfEvidenceStatusIsNotSubmittedOrDifferentOpinion()
+    //     external
+    // {}
+
     //////////////////////////////
     //    updateSkillDomains    //
     //////////////////////////////
@@ -172,6 +315,63 @@ contract VerifierTest is Test {
         vm.expectEmit(true, false, false, true, address(verifier));
         emit VerifierSkillDomainUpdated(USER, NEW_SKILL_DOMAINS);
         verifier.updateSkillDomains(NEW_SKILL_DOMAINS);
+    }
+
+    ///////////////////////////////
+    ///     provideFeedback     ///
+    ///////////////////////////////
+
+    function testProvideFeedbackRevertIfNotSelectedVerifiers() external {
+        _stakeToBeVerifier(USER);
+        vm.prank(USER);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Verifier.Verifier__NotSelectedVerifier.selector
+            )
+        );
+        verifier.provideFeedback(FEEDBACK_IPFS_HASH, IPFS_HASH, USER, true);
+    }
+
+    function testProvideFeedbackUpdateAddressToEvidences() external {
+        address[]
+            memory currentVerifiers = _createNumWordsNumberOfSameDomainVerifier(
+                SKILL_DOMAINS
+            );
+
+        StructDefinition.VSkillUserEvidence memory ev = StructDefinition
+            .VSkillUserEvidence(
+                USER,
+                IPFS_HASH,
+                SKILL_DOMAINS[0],
+                StructDefinition.VSkillUserSubmissionStatus.SUBMITTED,
+                new string[](0)
+            );
+        vm.startPrank(USER);
+        verifier.submitEvidence{
+            value: verifier.getSubmissionFeeInUsd().convertUsdToEth(
+                AggregatorV3Interface(verifierConstructorParams.priceFeed)
+            )
+        }(ev.evidenceIpfsHash, ev.skillDomain);
+        vm.stopPrank();
+
+        vm.recordLogs();
+        verifier._requestVerifiersSelection(ev);
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        bytes32 requestId = entries[0].topics[2];
+        VRFCoordinatorV2Mock vrfCoordinatorMock = VRFCoordinatorV2Mock(
+            verifierConstructorParams.vrfCoordinator
+        );
+        vm.pauseGasMetering();
+        vrfCoordinatorMock.fulfillRandomWords(
+            uint256(requestId),
+            address(verifier)
+        );
+
+        vm.prank(currentVerifiers[2]);
+        verifier.provideFeedback(FEEDBACK_IPFS_HASH, IPFS_HASH, USER, true);
+        StructDefinition.VSkillUserEvidence[] memory evidences = verifier
+            .getAddressToEvidences(USER);
+        assertEq(evidences.length, 1);
     }
 
     //////////////////////////////////////
