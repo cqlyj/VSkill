48f36ebd1cc45d415a10c477f97c8d1a42286a06
cqlyj <luoyingjie0721@gmail.com>
2024-10-12 13:40:56 +0800

    test those interactions

--- test/user/integration/InteractionsTest.t.sol
+++ test/user/integration/InteractionsTest.t.sol
@@ -4,17 +4,24 @@ pragma solidity ^0.8.24;
 
 import {Test, console} from "lib/forge-std/src/Test.sol";
 import {DeployVSkillUser} from "script/user/DeployVSkillUser.s.sol";
-import {SubmitEvidenceVSkill, ChangeSubmissionFeeVSkill, AddMoreSkillsVSkill} from "script/user/Interactions.s.sol";
+import {SubmitEvidenceVSkillUser, ChangeSubmissionFeeVSkillUser, AddMoreSkillsVSkillUser, CheckFeedbackOfEvidenceVSkillUser, EarnUserNft} from "script/user/Interactions.s.sol";
 import {VSkillUser} from "src/user/VSkillUser.sol";
 import {HelperConfig} from "script/user/HelperConfig.s.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {StructDefinition} from "src/utils/library/StructDefinition.sol";
 
 contract InteractionsTest is Test {
+    using StructDefinition for StructDefinition.VSkillUserEvidence;
+
     VSkillUser vskill;
     HelperConfig helperConfig;
     uint256 submissionFeeInUsd;
     string public constant NEW_SKILL_DOMAIN = "New skill domain";
     string public constant NEW_NFT_IMAGE_URI = "newnftimageuri";
     uint256 public constant NEW_SUBMISSION_FEE_IN_USD = 10e18; // 10 USD
+    string public constant IPFS_HASH =
+        "https://ipfs.io/ipfs/QmbJLndDmDiwdotu3MtfcjC2hC5tXeAR9EXbNSdUDUDYWa";
+    string public constant SKILL_DOMAIN = "Blockchain";
 
     function setUp() external {
         DeployVSkillUser deployer = new DeployVSkillUser();
@@ -22,21 +29,25 @@ contract InteractionsTest is Test {
         (submissionFeeInUsd, ) = helperConfig.activeNetworkConfig();
     }
 
-    function testInteractions() external {
-        SubmitEvidenceVSkill submitEvidence = new SubmitEvidenceVSkill();
-        submitEvidence.submitEvidenceVSkill(
+    function testVSkillUserInteractions() external {
+        SubmitEvidenceVSkillUser submitEvidence = new SubmitEvidenceVSkillUser();
+        vm.recordLogs();
+        submitEvidence.submitEvidenceVSkillUser(
             address(vskill),
             submissionFeeInUsd
         );
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        assertEq(entries.length, 2);
+        console.log("Evidence submitted successfully");
 
-        ChangeSubmissionFeeVSkill changeSubmissionFee = new ChangeSubmissionFeeVSkill();
-        changeSubmissionFee.changeSubmissionFeeVSkill(
+        ChangeSubmissionFeeVSkillUser changeSubmissionFee = new ChangeSubmissionFeeVSkillUser();
+        changeSubmissionFee.changeSubmissionFeeVSkillUser(
             address(vskill),
             NEW_SUBMISSION_FEE_IN_USD
         );
 
-        AddMoreSkillsVSkill addMoreSkills = new AddMoreSkillsVSkill();
-        addMoreSkills.addMoreSkillsVSkill(
+        AddMoreSkillsVSkillUser addMoreSkills = new AddMoreSkillsVSkillUser();
+        addMoreSkills.addMoreSkillsVSkillUser(
             address(vskill),
             NEW_SKILL_DOMAIN,
             NEW_NFT_IMAGE_URI
@@ -47,5 +58,21 @@ contract InteractionsTest is Test {
 
         assertEq(vskill.getSkillDomains().length, 6);
         console.log("New skills added successfully");
+
+        CheckFeedbackOfEvidenceVSkillUser checkFeedback = new CheckFeedbackOfEvidenceVSkillUser();
+        checkFeedback.checkFeedbackOfEvidenceVSkillUser(address(vskill), 0);
+
+        EarnUserNft earnNft = new EarnUserNft();
+        StructDefinition.VSkillUserEvidence
+            memory goodEvidence = StructDefinition.VSkillUserEvidence(
+                msg.sender,
+                IPFS_HASH,
+                SKILL_DOMAIN,
+                StructDefinition.VSkillUserSubmissionStatus.APPROVED,
+                new string[](0)
+            );
+        earnNft.earnUserNft(address(vskill), goodEvidence);
+
+        assertEq(vskill.balanceOf(msg.sender), 1);
     }
 }
