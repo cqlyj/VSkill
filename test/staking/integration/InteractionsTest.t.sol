c11e45d13180eea3a2d99052c5b17c3ce54a09ce
cqlyj <luoyingjie0721@gmail.com>
2024-10-12 12:52:25 +0800

    add the corresponding integration test for staking contract

--- test/staking/integration/InteractionsTest.t.sol
+++ test/staking/integration/InteractionsTest.t.sol
@@ -5,9 +5,11 @@ pragma solidity ^0.8.24;
 import {Test, console} from "lib/forge-std/src/Test.sol";
 import {Staking} from "src/staking/Staking.sol";
 import {DeployStaking} from "script/staking/DeployStaking.s.sol";
-import {WithdrawStakeStaking, StakeStaking} from "script/staking/Interactions.s.sol";
+import {PriceConverter} from "src/utils/library/PriceCoverter.sol";
+import {WithdrawStakeStaking, StakeStaking, AddBonusMoneyForVerifierStaking} from "script/staking/Interactions.s.sol";
 
 contract InteractionsTest is Test {
+    using PriceConverter for uint256;
     Staking staking;
 
     function setUp() external {
@@ -15,11 +17,12 @@ contract InteractionsTest is Test {
         (staking, ) = deployer.run();
     }
 
-    function testInteractions() external {
+    function testStakingInteractions() external {
         StakeStaking staker = new StakeStaking();
         staker.stakeStaking(address(staking));
 
         assertEq(staking.getVerifierCount(), 1);
+
         console.log("Stake success");
 
         WithdrawStakeStaking withdrawer = new WithdrawStakeStaking();
@@ -27,5 +30,14 @@ contract InteractionsTest is Test {
 
         assertEq(staking.getVerifierCount(), 0);
         console.log("Withdraw stake success");
+
+        AddBonusMoneyForVerifierStaking bonus = new AddBonusMoneyForVerifierStaking();
+        bonus.addBonusMoneyForVerifierStaking(address(staking));
+
+        assertEq(
+            staking.getBonusMoneyInEth(),
+            staking.getMinUsdAmount().convertUsdToEth(staking.getPriceFeed())
+        );
+        console.log("Add bonus success");
     }
 }
