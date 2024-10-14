39e4a91efb7b9f3938ddc8ab9261968f270ba5ee
cqlyj <luoyingjie0721@gmail.com>
2024-10-14 08:43:11 +0800

    Add script to mint user NFT for skill domain

---
+++ script/nft/Interactions.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {VSkillUserNft} from "src/nft/VSkillUserNft.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+pragma solidity ^0.8.24;
+
+contract MintUserNftVSkillUserNft is Script {
+    string public skillDomain;
+
+    function mintUserNftVSkillUserNft(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        VSkillUserNft vSkillUserNft = VSkillUserNft(mostRecentlyDeployed);
+        string[] memory skillDomains = vSkillUserNft.getSkillDomains();
+        vSkillUserNft.mintUserNft(skillDomains[0]);
+        vm.stopBroadcast();
+
+        console.log("Minted user NFT for skill domain: ", skillDomains[0]);
+        console.log("User NFT minted successfully");
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "VSkillUserNft",
+            block.chainid
+        );
+
+        console.log("Most recently deployed address: ", mostRecentlyDeployed);
+        mintUserNftVSkillUserNft(mostRecentlyDeployed);
+    }
+}
