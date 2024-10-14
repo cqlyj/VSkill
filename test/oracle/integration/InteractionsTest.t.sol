eeb410dcba22e7821f255a0bd06728bbda85f4f2
cqlyj <luoyingjie0721@gmail.com>
2024-10-14 09:08:08 +0800

    Add Distribution interactions and tests for InteractionsTest

--- test/oracle/integration/InteractionsTest.t.sol
+++ test/oracle/integration/InteractionsTest.t.sol
@@ -3,5 +3,28 @@
 pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
+import {Distribution} from "src/oracle/Distribution.sol";
+import {DeployDistribution} from "script/oracle/DeployDistribution.s.sol";
+import {HelperConfig} from "script/oracle/HelperConfig.s.sol";
+import {DistributionRandomNumberForVerifiersDistribution} from "script/oracle/Interactions.s.sol";
+import {Vm} from "forge-std/Vm.sol";
 
-contract InteractionsTest is Test {}
+contract InteractionsTest is Test {
+    DeployDistribution deployDistribution;
+    HelperConfig helperConfig;
+    Distribution distribution;
+
+    function setUp() external {
+        deployDistribution = new DeployDistribution();
+        (distribution, helperConfig) = deployDistribution.run();
+    }
+
+    function testInteractionsDistribution() external {
+        DistributionRandomNumberForVerifiersDistribution distributionRandomNumberForVerifiersDistribution = new DistributionRandomNumberForVerifiersDistribution();
+        vm.recordLogs();
+        distributionRandomNumberForVerifiersDistribution
+            .distributionRandomNumberForVerifiers(address(distribution));
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        assertEq(entries.length, 2);
+    }
+}
