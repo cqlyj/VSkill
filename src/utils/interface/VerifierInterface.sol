9eb750d95ff4725ec018030d4856207f6d6c0e87
cqlyj <luoyingjie0721@gmail.com>
2024-10-15 15:58:51 +0800

    add netspect for the VerifierInterface

--- src/utils/interface/VerifierInterface.sol
+++ src/utils/interface/VerifierInterface.sol
@@ -3,6 +3,11 @@
 pragma solidity ^0.8.24;
 import {StructDefinition} from "../library/StructDefinition.sol";
 
+/**
+ * @title VerifierInterfacev that will be implemented by the Verifier contract
+ * @author Luo Yingjie
+ * @notice This interface is for the Distribution contract call the Verifier contract callback function => _selectedVerifiersAddressCallback
+ */
 interface VerifierInterface {
     function _selectedVerifiersAddressCallback(
         StructDefinition.VSkillUserEvidence memory ev,
