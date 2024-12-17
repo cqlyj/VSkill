0a12540c71f644a91d8c38621a9b70c5f9193fd7
cqlyj <luoyingjie0721@gmail.com>
2024-12-17 16:34:37 +0800

    update the comment to get rid of the warnings

--- test/v1/staking/unit/StakingTest.t.sol
+++ test/v1/staking/unit/StakingTest.t.sol
@@ -1,6 +1,6 @@
-// // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
-// pragma solidity ^0.8.24;
+pragma solidity ^0.8.24;
 
 // import {Test, console} from "lib/forge-std/src/Test.sol";
 // import {Staking} from "src/staking/Staking.sol";
