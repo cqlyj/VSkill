a23be4a4e2aa60f664e3956f774973fbd76b2689
cqlyj <luoyingjie0721@gmail.com>
2025-01-03 00:40:59 +0800

    Refactor VSkillUserNft contract to include AccessControl, improve skill domain validation, and update event emissions

--- src/nft/VSkillUserNft.sol
+++ src/nft/VSkillUserNft.sol
@@ -1,85 +1,109 @@
 // SPDX-License-Identifier: MIT
 
-// @written audit-info floating pragma
 pragma solidity 0.8.26;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-/**
- * @title VSkillUserNft contract for minting user NFTs with skill domains
- * @author Luo Yingjie
- * @notice This is the contract for minting user NFTs with skill domains
- * @dev The user NFTs are minted with skill domains, it's a ERC721 svg NFT
- */
-contract VSkillUserNft is ERC721 {
+contract VSkillUserNft is ERC721, AccessControl {
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+
     uint256 private s_tokenCounter;
-    string[] private s_skillDomains = [
-        "Frontend",
-        "Backend",
-        "Fullstack",
-        "DevOps",
-        "Blockchain"
-    ];
+    string[] private s_skillDomains;
     string[] private s_userNftImageUris;
     mapping(string => string) private s_skillDomainToUserNftImageUri;
     mapping(uint256 => string) private s_tokenIdToSkillDomain;
 
-    ////////////////
-    //   Events   //
-    ////////////////
-    event MintNftSuccess(uint256 indexed tokenId, string indexed skillDomain);
+    bytes32 private constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 private constant SKILL_DOMAIN_ADDER_ROLE =
+        keccak256("SKILL_DOMAIN_ADDER_ROLE");
+
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    error VSkillUserNft__InvalidSkillDomain();
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    event VSkillUserNftMinted(
+        uint256 indexed tokenId,
+        string indexed skillDomain
+    );
     event SkillDomainsForNftAdded(
         string indexed newSkillDomain,
         string indexed newNftImageUri
     );
 
+    /*//////////////////////////////////////////////////////////////
+                              CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    // Later we will set the Relayer contract as the owner of this contract
+    // That is the minter will be the Relayer contract address
     constructor(
+        address minter,
+        string[] memory _skillDomains,
         string[] memory _userNftImageUris
     ) ERC721("VSkillUserNft", "VSU") {
         s_tokenCounter = 0;
         s_userNftImageUris = _userNftImageUris;
+        s_skillDomains = _skillDomains;
         uint256 skillDomainLength = s_skillDomains.length;
-        // e length here is just 5, it's fine
+        // length here will not be too large, so it's okay to use for loop
         for (uint256 i = 0; i < skillDomainLength; i++) {
             s_skillDomainToUserNftImageUri[
                 s_skillDomains[i]
             ] = s_userNftImageUris[i];
         }
+
+        _grantRole(SKILL_DOMAIN_ADDER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, minter);
     }
 
-    /**
-     *
-     * @param skillDomain The domain of the skill
-     * @dev Mint a user NFT with the skill domain
-     */
+    /*//////////////////////////////////////////////////////////////
+                           OVERRIDE FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
-    // @written audit-low This function not checking for the skillDomain input, users can mint NFT with non-existing skill domains, not a big deal
-    // @written audit-high This function is not restricted to any specific user, anyone can mint a NFT
-    // Users can directly call this function to mint a NFT with any skill domain instead of providing a proof of skill
-    function mintUserNft(string memory skillDomain) public {
-        _safeMint(msg.sender, s_tokenCounter);
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                     EXTERNAL AND PUBLIC FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function mintUserNft(
+        string memory skillDomain
+    ) public onlyRole(MINTER_ROLE) {
+        if (!_validSkillDomain(skillDomain)) {
+            revert VSkillUserNft__InvalidSkillDomain();
+        }
+
         s_tokenIdToSkillDomain[s_tokenCounter] = skillDomain;
         s_tokenCounter++;
+        _safeMint(msg.sender, s_tokenCounter - 1);
 
-        emit MintNftSuccess(s_tokenCounter - 1, skillDomain);
+        emit VSkillUserNftMinted(s_tokenCounter - 1, skillDomain);
     }
 
-    /**
-     *
-     * @param tokenId The id of the token
-     * @return The URI of the token
-     * @dev Get the URI of the token
-     */
     function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
-        string memory skillDomain = s_tokenIdToSkillDomain[tokenId];
-        // what if skillDomain is not found?
-        // imageUri will not revert, but just be blank
-        // @written audit-info/low if the tokenId is not found, the function will return a blank string
-        string memory imageUri = s_skillDomainToUserNftImageUri[skillDomain];
+        // If the token is not minted yet, return empty string
+        if (tokenId >= s_tokenCounter) {
+            return "";
+        }
 
+        string memory skillDomain = s_tokenIdToSkillDomain[tokenId];
+        string memory imageUri = s_skillDomainToUserNftImageUri[skillDomain];
         return
             string(
                 abi.encodePacked(
@@ -100,19 +124,10 @@ contract VSkillUserNft is ERC721 {
             );
     }
 
-    ///////////////////////////////
-    ///    Internal Functions   ///
-    ///////////////////////////////
-
-    /**
-     *
-     * @param skillDomain The domain of the skill
-     * @param newNftImageUri New NFT image URI for the new skill domain
-     */
-    function _addMoreSkillsForNft(
+    function addMoreSkillsForNft(
         string memory skillDomain,
         string memory newNftImageUri
-    ) public {
+    ) public onlyRole(SKILL_DOMAIN_ADDER_ROLE) {
         s_skillDomains.push(skillDomain);
         s_userNftImageUris.push(newNftImageUri);
         s_skillDomainToUserNftImageUri[skillDomain] = newNftImageUri;
@@ -120,17 +135,32 @@ contract VSkillUserNft is ERC721 {
         emit SkillDomainsForNftAdded(skillDomain, newNftImageUri);
     }
 
-    /**
-     * @return The base URI of the token
-     * @dev Get the base URI of the token
-     */
+    /*//////////////////////////////////////////////////////////////
+                     INTERNAL AND PRIVATE FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
     function _baseURI() internal pure override returns (string memory) {
         return "data:application/json;base64,";
     }
 
-    ///////////////////////////////
-    /////   Getter Functions   ////
-    ///////////////////////////////
+    function _validSkillDomain(
+        string memory skillDomain
+    ) internal view returns (bool) {
+        uint256 skillDomainLength = s_skillDomains.length;
+        for (uint256 i = 0; i < skillDomainLength; i++) {
+            if (
+                keccak256(abi.encodePacked(s_skillDomains[i])) ==
+                keccak256(abi.encodePacked(skillDomain))
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                GETTERS
+    //////////////////////////////////////////////////////////////*/
 
     function getTokenCounter() external view returns (uint256) {
         return s_tokenCounter;
@@ -160,3 +190,11 @@ contract VSkillUserNft is ERC721 {
         return _baseURI();
     }
 }
+
+// string[] private s_skillDomains = [
+//     "Frontend",
+//     "Backend",
+//     "Fullstack",
+//     "DevOps",
+//     "Blockchain"
+// ];
