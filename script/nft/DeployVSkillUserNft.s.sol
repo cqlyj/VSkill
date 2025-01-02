4d883efd3026188390394f789ec0e84e64d3dd29
cqlyj <luoyingjie0721@gmail.com>
2025-01-03 00:40:51 +0800

    comment some contracts to get rid of errors when refactoring the VSkillUserNft contract

--- script/nft/DeployVSkillUserNft.s.sol
+++ script/nft/DeployVSkillUserNft.s.sol
@@ -2,19 +2,19 @@
 
 pragma solidity ^0.8.24;
 
-import {Script} from "forge-std/Script.sol";
-import {VSkillUserNft} from "../../src/nft/VSkillUserNft.sol";
-import {HelperConfig} from "./HelperConfig.s.sol";
+// import {Script} from "forge-std/Script.sol";
+// import {VSkillUserNft} from "../../src/nft/VSkillUserNft.sol";
+// import {HelperConfig} from "./HelperConfig.s.sol";
 
-contract DeployVSkillUserNft is Script {
-    function run() external returns (VSkillUserNft, HelperConfig) {
-        HelperConfig helperConfig = new HelperConfig();
-        string[] memory userNftImageUris = helperConfig
-            .getActiveNetworkConfig()
-            .userNftImageUris;
-        vm.startBroadcast();
-        VSkillUserNft vskillUserNft = new VSkillUserNft(userNftImageUris);
-        vm.stopBroadcast();
-        return (vskillUserNft, helperConfig);
-    }
-}
+// contract DeployVSkillUserNft is Script {
+//     function run() external returns (VSkillUserNft, HelperConfig) {
+//         HelperConfig helperConfig = new HelperConfig();
+//         string[] memory userNftImageUris = helperConfig
+//             .getActiveNetworkConfig()
+//             .userNftImageUris;
+//         vm.startBroadcast();
+//         VSkillUserNft vskillUserNft = new VSkillUserNft(userNftImageUris);
+//         vm.stopBroadcast();
+//         return (vskillUserNft, helperConfig);
+//     }
+// }
