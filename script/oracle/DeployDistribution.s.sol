bdd12d6488a2a2efe1028574c5a9734167c985ff
cqlyj <luoyingjie0721@gmail.com>
2025-01-03 22:55:22 +0800

    Comment out the DeployDistribution contract and its dependencies for now not interfere with the development

--- script/oracle/DeployDistribution.s.sol
+++ script/oracle/DeployDistribution.s.sol
@@ -2,59 +2,59 @@
 
 pragma solidity ^0.8.24;
 
-import {Script} from "forge-std/Script.sol";
-import {Distribution} from "../../src/oracle/Distribution.sol";
-import {HelperConfig} from "./HelperConfig.s.sol";
-import {CreateSubscriptionDistribution, FundSubscriptionDistribution, AddConsumerDistribution} from "./Interactions.s.sol";
+// import {Script} from "forge-std/Script.sol";
+// import {Distribution} from "../../src/oracle/Distribution.sol";
+// import {HelperConfig} from "./HelperConfig.s.sol";
+// import {CreateSubscriptionDistribution, FundSubscriptionDistribution, AddConsumerDistribution} from "./Interactions.s.sol";
 
-contract DeployDistribution is Script {
-    function run() external returns (Distribution, HelperConfig) {
-        HelperConfig helperConfig = new HelperConfig();
-        (
-            uint64 subscriptionId,
-            address vrfCoordinator,
-            bytes32 keyHash,
-            uint32 callbackGasLimit,
-            address linkTokenAddress,
-            uint256 deployerKey
-        ) = helperConfig.activeNetworkConfig();
+// contract DeployDistribution is Script {
+//     function run() external returns (Distribution, HelperConfig) {
+//         HelperConfig helperConfig = new HelperConfig();
+//         (
+//             uint64 subscriptionId,
+//             address vrfCoordinator,
+//             bytes32 keyHash,
+//             uint32 callbackGasLimit,
+//             address linkTokenAddress,
+//             uint256 deployerKey
+//         ) = helperConfig.activeNetworkConfig();
 
-        if (subscriptionId == 0) {
-            // Create subscription
-            CreateSubscriptionDistribution subscription = new CreateSubscriptionDistribution();
-            subscriptionId = subscription.createSubscription(
-                vrfCoordinator,
-                deployerKey
-            );
+//         if (subscriptionId == 0) {
+//             // Create subscription
+//             CreateSubscriptionDistribution subscription = new CreateSubscriptionDistribution();
+//             subscriptionId = subscription.createSubscription(
+//                 vrfCoordinator,
+//                 deployerKey
+//             );
 
-            // Fund subscription
-            FundSubscriptionDistribution fund = new FundSubscriptionDistribution();
-            fund.fundSubscription(
-                vrfCoordinator,
-                subscriptionId,
-                linkTokenAddress,
-                deployerKey
-            );
-        }
+//             // Fund subscription
+//             FundSubscriptionDistribution fund = new FundSubscriptionDistribution();
+//             fund.fundSubscription(
+//                 vrfCoordinator,
+//                 subscriptionId,
+//                 linkTokenAddress,
+//                 deployerKey
+//             );
+//         }
 
-        vm.startBroadcast();
-        Distribution distribution = new Distribution(
-            subscriptionId,
-            vrfCoordinator,
-            keyHash,
-            callbackGasLimit
-        );
-        vm.stopBroadcast();
+//         vm.startBroadcast();
+//         Distribution distribution = new Distribution(
+//             subscriptionId,
+//             vrfCoordinator,
+//             keyHash,
+//             callbackGasLimit
+//         );
+//         vm.stopBroadcast();
 
-        // Add consumer
-        AddConsumerDistribution consumer = new AddConsumerDistribution();
-        consumer.addConsumer(
-            address(distribution),
-            vrfCoordinator,
-            subscriptionId,
-            deployerKey
-        );
+//         // Add consumer
+//         AddConsumerDistribution consumer = new AddConsumerDistribution();
+//         consumer.addConsumer(
+//             address(distribution),
+//             vrfCoordinator,
+//             subscriptionId,
+//             deployerKey
+//         );
 
-        return (distribution, helperConfig);
-    }
-}
+//         return (distribution, helperConfig);
+//     }
+// }
--- script/oracle/Interactions.s.sol
+++ script/oracle/Interactions.s.sol
@@ -2,196 +2,196 @@
 
 pragma solidity ^0.8.24;
 
-import {Script, console} from "forge-std/Script.sol";
-import {HelperConfig} from "./HelperConfig.s.sol";
-import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2Mock.sol";
-import {MockLinkToken} from "@chainlink/contracts/src/v0.8/mocks/MockLinkToken.sol";
-import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
-import {Distribution} from "src/oracle/Distribution.sol";
-import {StructDefinition} from "src/utils/library/StructDefinition.sol";
+// import {Script, console} from "forge-std/Script.sol";
+// import {HelperConfig} from "./HelperConfig.s.sol";
+// import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2Mock.sol";
+// import {MockLinkToken} from "@chainlink/contracts/src/v0.8/mocks/MockLinkToken.sol";
+// import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+// import {Distribution} from "src/oracle/Distribution.sol";
+// import {StructDefinition} from "src/utils/library/StructDefinition.sol";
 
-contract CreateSubscriptionDistribution is Script {
-    function createSubscriptionWithConfig() internal returns (uint64) {
-        HelperConfig helperConfig = new HelperConfig();
-        (, address vrfCoordinator, , , , uint256 deployerKey) = helperConfig
-            .activeNetworkConfig();
-        return createSubscription(vrfCoordinator, deployerKey);
-    }
+// contract CreateSubscriptionDistribution is Script {
+//     function createSubscriptionWithConfig() internal returns (uint64) {
+//         HelperConfig helperConfig = new HelperConfig();
+//         (, address vrfCoordinator, , , , uint256 deployerKey) = helperConfig
+//             .activeNetworkConfig();
+//         return createSubscription(vrfCoordinator, deployerKey);
+//     }
 
-    function createSubscription(
-        address vrfCoordinator,
-        uint256 deployerKey
-    ) public returns (uint64) {
-        console.log("Creating subscription on chainid: ", block.chainid);
+//     function createSubscription(
+//         address vrfCoordinator,
+//         uint256 deployerKey
+//     ) public returns (uint64) {
+//         console.log("Creating subscription on chainid: ", block.chainid);
 
-        vm.startBroadcast(deployerKey);
+//         vm.startBroadcast(deployerKey);
 
-        uint64 subId = VRFCoordinatorV2Mock(vrfCoordinator)
-            .createSubscription();
+//         uint64 subId = VRFCoordinatorV2Mock(vrfCoordinator)
+//             .createSubscription();
 
-        vm.stopBroadcast();
+//         vm.stopBroadcast();
 
-        console.log("Subscription ID: ", subId);
-        console.log(
-            "Please update the subscription ID in the HelperConfig contract!"
-        );
-        return subId;
-    }
+//         console.log("Subscription ID: ", subId);
+//         console.log(
+//             "Please update the subscription ID in the HelperConfig contract!"
+//         );
+//         return subId;
+//     }
 
-    function run() external returns (uint64) {
-        return createSubscriptionWithConfig();
-    }
-}
+//     function run() external returns (uint64) {
+//         return createSubscriptionWithConfig();
+//     }
+// }
 
-contract FundSubscriptionDistribution is Script {
-    uint96 public constant FUND_AMOUNT = 3 ether;
+// contract FundSubscriptionDistribution is Script {
+//     uint96 public constant FUND_AMOUNT = 3 ether;
 
-    function fundSubscriptionWithConfig() internal {
-        HelperConfig helperConfig = new HelperConfig();
-        (
-            uint64 subscriptionId,
-            address vrfCoordinator,
-            ,
-            ,
-            address linkTokenAddress,
-            uint256 deployerKey
-        ) = helperConfig.activeNetworkConfig();
+//     function fundSubscriptionWithConfig() internal {
+//         HelperConfig helperConfig = new HelperConfig();
+//         (
+//             uint64 subscriptionId,
+//             address vrfCoordinator,
+//             ,
+//             ,
+//             address linkTokenAddress,
+//             uint256 deployerKey
+//         ) = helperConfig.activeNetworkConfig();
 
-        fundSubscription(
-            vrfCoordinator,
-            subscriptionId,
-            linkTokenAddress,
-            deployerKey
-        );
-    }
+//         fundSubscription(
+//             vrfCoordinator,
+//             subscriptionId,
+//             linkTokenAddress,
+//             deployerKey
+//         );
+//     }
 
-    function fundSubscription(
-        address vrfCoordinator,
-        uint64 subscriptionId,
-        address linkTokenAddress,
-        uint256 deployerKey
-    ) public {
-        console.log("Funding subscription: ", subscriptionId);
-        console.log("Using vrfCoordinator: ", vrfCoordinator);
-        console.log("ChainId:", block.chainid);
+//     function fundSubscription(
+//         address vrfCoordinator,
+//         uint64 subscriptionId,
+//         address linkTokenAddress,
+//         uint256 deployerKey
+//     ) public {
+//         console.log("Funding subscription: ", subscriptionId);
+//         console.log("Using vrfCoordinator: ", vrfCoordinator);
+//         console.log("ChainId:", block.chainid);
 
-        if (block.chainid == 31337) {
-            vm.startBroadcast(deployerKey);
+//         if (block.chainid == 31337) {
+//             vm.startBroadcast(deployerKey);
 
-            VRFCoordinatorV2Mock(vrfCoordinator).fundSubscription(
-                subscriptionId,
-                FUND_AMOUNT
-            );
+//             VRFCoordinatorV2Mock(vrfCoordinator).fundSubscription(
+//                 subscriptionId,
+//                 FUND_AMOUNT
+//             );
 
-            vm.stopBroadcast();
-        } else {
-            vm.startBroadcast(deployerKey);
+//             vm.stopBroadcast();
+//         } else {
+//             vm.startBroadcast(deployerKey);
 
-            MockLinkToken(linkTokenAddress).transferAndCall(
-                vrfCoordinator,
-                FUND_AMOUNT,
-                abi.encode(subscriptionId)
-            );
+//             MockLinkToken(linkTokenAddress).transferAndCall(
+//                 vrfCoordinator,
+//                 FUND_AMOUNT,
+//                 abi.encode(subscriptionId)
+//             );
 
-            vm.stopBroadcast();
-        }
-    }
+//             vm.stopBroadcast();
+//         }
+//     }
 
-    function run() external {
-        fundSubscriptionWithConfig();
-    }
-}
+//     function run() external {
+//         fundSubscriptionWithConfig();
+//     }
+// }
 
-contract AddConsumerDistribution is Script {
-    function addConsumerUsingConfig(address mostRecentlyDeployed) internal {
-        HelperConfig helperConfig = new HelperConfig();
-        (
-            uint64 subscriptionId,
-            address vrfCoordinator,
-            ,
-            ,
-            ,
-            uint256 deployerKey
-        ) = helperConfig.activeNetworkConfig();
-        addConsumer(
-            mostRecentlyDeployed,
-            vrfCoordinator,
-            subscriptionId,
-            deployerKey
-        );
-    }
+// contract AddConsumerDistribution is Script {
+//     function addConsumerUsingConfig(address mostRecentlyDeployed) internal {
+//         HelperConfig helperConfig = new HelperConfig();
+//         (
+//             uint64 subscriptionId,
+//             address vrfCoordinator,
+//             ,
+//             ,
+//             ,
+//             uint256 deployerKey
+//         ) = helperConfig.activeNetworkConfig();
+//         addConsumer(
+//             mostRecentlyDeployed,
+//             vrfCoordinator,
+//             subscriptionId,
+//             deployerKey
+//         );
+//     }
 
-    function addConsumer(
-        address mostRecentlyDeployed,
-        address vrfCoordinator,
-        uint64 subscriptionId,
-        uint256 deployerKey
-    ) public {
-        console.log("Adding consumer to Distribution: ", mostRecentlyDeployed);
-        console.log("Using vrfCoordinator: ", vrfCoordinator);
-        console.log("On chainId: ", block.chainid);
-        console.log("Subscription ID: ", subscriptionId);
+//     function addConsumer(
+//         address mostRecentlyDeployed,
+//         address vrfCoordinator,
+//         uint64 subscriptionId,
+//         uint256 deployerKey
+//     ) public {
+//         console.log("Adding consumer to Distribution: ", mostRecentlyDeployed);
+//         console.log("Using vrfCoordinator: ", vrfCoordinator);
+//         console.log("On chainId: ", block.chainid);
+//         console.log("Subscription ID: ", subscriptionId);
 
-        vm.startBroadcast(deployerKey);
+//         vm.startBroadcast(deployerKey);
 
-        VRFCoordinatorV2Mock(vrfCoordinator).addConsumer(
-            subscriptionId,
-            mostRecentlyDeployed
-        );
+//         VRFCoordinatorV2Mock(vrfCoordinator).addConsumer(
+//             subscriptionId,
+//             mostRecentlyDeployed
+//         );
 
-        vm.stopBroadcast();
-    }
+//         vm.stopBroadcast();
+//     }
 
-    function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
-            "Distribution",
-            block.chainid
-        );
+//     function run() external {
+//         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+//             "Distribution",
+//             block.chainid
+//         );
 
-        addConsumerUsingConfig(mostRecentlyDeployed);
-    }
-}
+//         addConsumerUsingConfig(mostRecentlyDeployed);
+//     }
+// }
 
-contract DistributionRandomNumberForVerifiersDistribution is Script {
-    using StructDefinition for StructDefinition.VSkillUserEvidence;
-    using StructDefinition for StructDefinition.VSkillUserSubmissionStatus;
+// contract DistributionRandomNumberForVerifiersDistribution is Script {
+//     using StructDefinition for StructDefinition.VSkillUserEvidence;
+//     using StructDefinition for StructDefinition.VSkillUserSubmissionStatus;
 
-    address public requester = makeAddr("requester");
-    address public submitter = makeAddr("submitter");
-    string constant EVIDENCE_IPFS_HASH = "evidenceIpfsHash";
-    string constant SKILLDOMAIN = "frontEnd";
-    StructDefinition.VSkillUserSubmissionStatus status =
-        StructDefinition.VSkillUserSubmissionStatus.SUBMITTED;
-    string[] feedbackIpfsHash;
-    StructDefinition.VSkillUserEvidence public ev =
-        StructDefinition.VSkillUserEvidence(
-            submitter,
-            EVIDENCE_IPFS_HASH,
-            SKILLDOMAIN,
-            status,
-            feedbackIpfsHash
-        );
+//     address public requester = makeAddr("requester");
+//     address public submitter = makeAddr("submitter");
+//     string constant EVIDENCE_IPFS_HASH = "evidenceIpfsHash";
+//     string constant SKILLDOMAIN = "frontEnd";
+//     StructDefinition.VSkillUserSubmissionStatus status =
+//         StructDefinition.VSkillUserSubmissionStatus.SUBMITTED;
+//     string[] feedbackIpfsHash;
+//     StructDefinition.VSkillUserEvidence public ev =
+//         StructDefinition.VSkillUserEvidence(
+//             submitter,
+//             EVIDENCE_IPFS_HASH,
+//             SKILLDOMAIN,
+//             status,
+//             feedbackIpfsHash
+//         );
 
-    function distributionRandomNumberForVerifiers(
-        address mostRecentlyDeployed
-    ) public {
-        vm.startBroadcast();
-        Distribution distribution = Distribution(mostRecentlyDeployed);
-        distribution.distributionRandomNumberForVerifiers(requester, ev);
-        vm.stopBroadcast();
-    }
+//     function distributionRandomNumberForVerifiers(
+//         address mostRecentlyDeployed
+//     ) public {
+//         vm.startBroadcast();
+//         Distribution distribution = Distribution(mostRecentlyDeployed);
+//         distribution.distributionRandomNumberForVerifiers(requester, ev);
+//         vm.stopBroadcast();
+//     }
 
-    function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
-            "Distribution",
-            block.chainid
-        );
+//     function run() external {
+//         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+//             "Distribution",
+//             block.chainid
+//         );
 
-        console.log(
-            "Using most recently deployed Distribution: ",
-            mostRecentlyDeployed
-        );
+//         console.log(
+//             "Using most recently deployed Distribution: ",
+//             mostRecentlyDeployed
+//         );
 
-        distributionRandomNumberForVerifiers(mostRecentlyDeployed);
-    }
-}
+//         distributionRandomNumberForVerifiers(mostRecentlyDeployed);
+//     }
+// }
