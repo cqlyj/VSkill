214cd5e3ebedb17a3c0f80220a34a5e465fcdb47
cqlyj <luoyingjie0721@gmail.com>
2025-01-19 23:54:45 +0800

    move the mock contract to the test folder

--- script/staking/HelperConfig.s.sol
+++ script/staking/HelperConfig.s.sol
@@ -2,62 +2,62 @@
 
 pragma solidity ^0.8.24;
 
-import {Script} from "../../lib/forge-std/src/Script.sol";
-import {MockV3Aggregator} from "../../test/v1/mock/MockV3Aggregator.sol";
+// import {Script} from "../../lib/forge-std/src/Script.sol";
+// import {MockV3Aggregator} from "../../test/v1/mock/MockV3Aggregator.sol";
 
-contract HelperConfig is Script {
-    struct NetworkConfig {
-        address priceFeed;
-    }
+// contract HelperConfig is Script {
+//     struct NetworkConfig {
+//         address priceFeed;
+//     }
 
-    uint8 private constant DECIMALS = 8;
-    int256 private constant INITIAL_ANSWER = 2000e8;
+//     uint8 private constant DECIMALS = 8;
+//     int256 private constant INITIAL_ANSWER = 2000e8;
 
-    NetworkConfig public activeNetworkConfig;
+//     NetworkConfig public activeNetworkConfig;
 
-    constructor() {
-        if (block.chainid == 1) {
-            activeNetworkConfig = getMainnetConfig();
-        } else if (block.chainid == 11155111) {
-            activeNetworkConfig = getSepoliaConfig();
-        } else {
-            activeNetworkConfig = getOrCreateAnvilChainConfig();
-        }
-    }
+//     constructor() {
+//         if (block.chainid == 1) {
+//             activeNetworkConfig = getMainnetConfig();
+//         } else if (block.chainid == 11155111) {
+//             activeNetworkConfig = getSepoliaConfig();
+//         } else {
+//             activeNetworkConfig = getOrCreateAnvilChainConfig();
+//         }
+//     }
 
-    function getSepoliaConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory sepoliaConfig = NetworkConfig({
-            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        });
-        return sepoliaConfig;
-    }
+//     function getSepoliaConfig() public pure returns (NetworkConfig memory) {
+//         NetworkConfig memory sepoliaConfig = NetworkConfig({
+//             priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+//         });
+//         return sepoliaConfig;
+//     }
 
-    function getMainnetConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory mainnetConfig = NetworkConfig({
-            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
-        });
-        return mainnetConfig;
-    }
+//     function getMainnetConfig() public pure returns (NetworkConfig memory) {
+//         NetworkConfig memory mainnetConfig = NetworkConfig({
+//             priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+//         });
+//         return mainnetConfig;
+//     }
 
-    function getOrCreateAnvilChainConfig()
-        public
-        returns (NetworkConfig memory)
-    {
-        if (activeNetworkConfig.priceFeed != address(0)) {
-            return activeNetworkConfig;
-        }
-        // deploy mock price feed contract
-        vm.startBroadcast();
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
-            DECIMALS,
-            INITIAL_ANSWER
-        );
-        vm.stopBroadcast();
+//     function getOrCreateAnvilChainConfig()
+//         public
+//         returns (NetworkConfig memory)
+//     {
+//         if (activeNetworkConfig.priceFeed != address(0)) {
+//             return activeNetworkConfig;
+//         }
+//         // deploy mock price feed contract
+//         vm.startBroadcast();
+//         MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+//             DECIMALS,
+//             INITIAL_ANSWER
+//         );
+//         vm.stopBroadcast();
 
-        NetworkConfig memory anvilChainConfig = NetworkConfig({
-            priceFeed: address(mockPriceFeed)
-        });
+//         NetworkConfig memory anvilChainConfig = NetworkConfig({
+//             priceFeed: address(mockPriceFeed)
+//         });
 
-        return anvilChainConfig;
-    }
-}
+//         return anvilChainConfig;
+//     }
+// }
