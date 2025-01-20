22f9e0205f25c7c8a69760cd2d69873e4537033f
cqlyj <luoyingjie0721@gmail.com>
2025-01-20 17:52:13 +0800

    Refactor DeployDistribution script to utilize new VRF interaction scripts for subscription management and funding

--- script/deploy/DeployDistribution.s.sol
+++ script/deploy/DeployDistribution.s.sol
@@ -5,39 +5,39 @@ pragma solidity 0.8.26;
 import {Script, console} from "forge-std/Script.sol";
 import {Distribution} from "src/Distribution.sol";
 import {DistributionHelperConfig} from "../helperConfig/DistributionHelperConfig.s.sol";
+import {AddConsumer} from "../interactions/VRFInteractions/AddConsumer.s.sol";
+import {CreateSubscription} from "../interactions/VRFInteractions/CreateSubscription.s.sol";
+import {FundSubscription} from "../interactions/VRFInteractions/FundSubscription.s.sol";
 
 // Before deploy this script, please make sure that the subscription is created and funded
 contract DeployDistribution is Script {
     function run() external returns (Distribution, DistributionHelperConfig) {
         DistributionHelperConfig helperConfig = new DistributionHelperConfig();
         (
-            uint64 subscriptionId,
+            uint256 subscriptionId,
             address vrfCoordinator,
             bytes32 keyHash,
-            uint32 callbackGasLimit
+            uint32 callbackGasLimit,
+            address linkToken
         ) = helperConfig.activeNetworkConfig();
 
         // these below are commented out because they are not needed in the script for deployment
         // they should be handled in the script for interactions before this deployment script is run
         // @update these below are commented out because they are not needed in the script for deployment
 
-        // if (subscriptionId == 0) {
-        //     // Create subscription
-        //     CreateSubscriptionDistribution subscription = new CreateSubscriptionDistribution();
-        //     subscriptionId = subscription.createSubscription(
-        //         vrfCoordinator,
-        //         deployerKey
-        //     );
+        if (subscriptionId == 0) {
+            // Create subscription
+            CreateSubscription subscription = new CreateSubscription();
+            (subscriptionId, ) = subscription.createSubscription(
+                vrfCoordinator
+            );
 
-        //     // Fund subscription
-        //     FundSubscriptionDistribution fund = new FundSubscriptionDistribution();
-        //     fund.fundSubscription(
-        //         vrfCoordinator,
-        //         subscriptionId,
-        //         linkTokenAddress,
-        //         deployerKey
-        //     );
-        // }
+            helperConfig.setActiveNetworkSubscriptionId(subscriptionId);
+
+            // Fund subscription
+            FundSubscription fund = new FundSubscription();
+            fund.fundSubscription(vrfCoordinator, subscriptionId, linkToken);
+        }
 
         vm.startBroadcast();
         Distribution distribution = new Distribution(
@@ -51,13 +51,12 @@ contract DeployDistribution is Script {
         console.log("Distribution deployed at: ", address(distribution));
 
         // // Add consumer
-        // AddConsumerDistribution consumer = new AddConsumerDistribution();
-        // consumer.addConsumer(
-        //     address(distribution),
-        //     vrfCoordinator,
-        //     subscriptionId,
-        //     deployerKey
-        // );
+        AddConsumer consumer = new AddConsumer();
+        consumer.addConsumer(
+            address(distribution),
+            vrfCoordinator,
+            subscriptionId
+        );
 
         return (distribution, helperConfig);
     }
