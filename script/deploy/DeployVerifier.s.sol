ef3e39d275458d0bcd5b6c37bddad0aacbbf1821
cqlyj <luoyingjie0721@gmail.com>
2025-01-23 01:26:26 +0800

    separate the deploy and run function so that in the tests we can correctly reuse the deploy function

--- script/deploy/DeployVerifier.s.sol
+++ script/deploy/DeployVerifier.s.sol
@@ -5,21 +5,35 @@ pragma solidity ^0.8.24;
 import {Script, console} from "forge-std/Script.sol";
 import {Verifier} from "src/Verifier.sol";
 import {VerifierHelperConfig} from "../helperConfig/VerifierHelperConfig.s.sol";
+import {Vm} from "forge-std/Vm.sol";
 
 contract DeployVerifier is Script {
+    function deployVerifier(
+        address priceFeed,
+        string[] memory skillDomains,
+        address vSkillUser
+    ) public returns (Verifier) {
+        vm.startBroadcast();
+        Verifier verifier = new Verifier(priceFeed, skillDomains, vSkillUser);
+        vm.stopBroadcast();
+
+        console.log("Verifier deployed at: ", address(verifier));
+
+        return verifier;
+    }
+
     function run() external returns (Verifier, VerifierHelperConfig) {
         VerifierHelperConfig helperConfig = new VerifierHelperConfig();
         address priceFeed = helperConfig.getActiveNetworkConfig().priceFeed;
         string[] memory skillDomains = helperConfig
             .getActiveNetworkConfig()
             .skillDomains;
-        address vSkillUser = helperConfig.getActiveNetworkConfig().vSkillUser;
+        address vSkillUser = Vm(address(vm)).getDeployment(
+            "VSkillUser",
+            uint64(block.chainid)
+        );
 
-        vm.startBroadcast();
-        Verifier verifier = new Verifier(priceFeed, skillDomains, vSkillUser);
-        vm.stopBroadcast();
-
-        console.log("Verifier deployed at: ", address(verifier));
+        Verifier verifier = deployVerifier(priceFeed, skillDomains, vSkillUser);
 
         return (verifier, helperConfig);
     }
