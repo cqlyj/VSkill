3fd0e8dbb107e80ad5b272728017825809f20c2b
cqlyj <luoyingjie0721@gmail.com>
2025-01-23 22:50:55 +0800

    update the VSkillUser interactions

--- script/interactions/VSkillUserInteractions.s.sol
+++ script/interactions/VSkillUserInteractions.s.sol
@@ -2,199 +2,90 @@
 
 pragma solidity ^0.8.24;
 
-// import {Script, console} from "forge-std/Script.sol";
-// import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
-// import {VSkillUser} from "src/user/VSkillUser.sol";
-// import {HelperConfig} from "./HelperConfig.s.sol";
-// import {StructDefinition} from "src/utils/library/StructDefinition.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {VSkillUser} from "src/VSkillUser.sol";
+import {Vm} from "forge-std/Vm.sol";
 
-// contract SubmitEvidenceVSkillUser is Script {
-//     string public constant IPFS_HASH =
-//         "https://ipfs.io/ipfs/QmbJLndDmDiwdotu3MtfcjC2hC5tXeAR9EXbNSdUDUDYWa";
-//     string public constant SKILL_DOMAIN = "Blockchain";
+contract SubmitEvidence is Script {
+    // @notice: Update this cid and skillDomain to the cid and skillDomain of the evidence you want to submit!
+    string cid = "bafkreigpyuchim2hb25gfbtqygwdvhhqinko5vobimzstdq6lqwnxm6nza";
+    string skillDomain = "Blockchain";
 
-//     function submitEvidenceVSkillUser(
-//         address mostRecentlyDeployed,
-//         uint256 submissionFeeInUsd
-//     ) public {
-//         vm.startBroadcast();
-//         VSkillUser vskill = VSkillUser(payable(mostRecentlyDeployed));
-//         vskill.submitEvidence{value: submissionFeeInUsd}(
-//             IPFS_HASH,
-//             SKILL_DOMAIN
-//         );
-//         vm.stopBroadcast();
-//     }
+    function submitEvidence(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
 
-//     function run() external {
-//         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
-//             "VSkillUser",
-//             block.chainid
-//         );
+        VSkillUser vSkillUser = VSkillUser(payable(mostRecentlyDeployed));
+        uint256 submissionFeeInEth = vSkillUser.getSubmissionFeeInEth();
+        vSkillUser.submitEvidence{value: submissionFeeInEth}(cid, skillDomain);
 
-//         HelperConfig helperConfig = new HelperConfig();
-//         (uint256 submissionFeeInUsd, ) = helperConfig.activeNetworkConfig();
+        vm.stopBroadcast();
 
-//         console.log("Most recently deployed address: ", mostRecentlyDeployed);
-//         console.log(
-//             "Submission fee in USD: ",
-//             submissionFeeInUsd / 1e18,
-//             " USD"
-//         );
-//         submitEvidenceVSkillUser(mostRecentlyDeployed, submissionFeeInUsd);
-//     }
-// }
+        console.log(
+            "Evidence submitted successfully on chain id: ",
+            block.chainid
+        );
+    }
 
-// contract ChangeSubmissionFeeVSkillUser is Script {
-//     uint256 public constant NEW_SUBMISSION_FEE_IN_USD = 10e18; // 10 USD
+    function run() external {
+        address vSkillUser = Vm(address(vm)).getDeployment(
+            "VSkillUser",
+            uint64(block.chainid)
+        );
 
-//     function changeSubmissionFeeVSkillUser(
-//         address mostRecentlyDeployed,
-//         uint256 newSubmissionFee
-//     ) public {
-//         vm.startBroadcast();
-//         VSkillUser vskill = VSkillUser(payable(mostRecentlyDeployed));
-//         vskill.changeSubmissionFee(newSubmissionFee);
-//         vm.stopBroadcast();
-//         uint256 currentSubmissionFee = vskill.getSubmissionFeeInUsd();
-//         console.log(
-//             "New submission fee in USD: ",
-//             currentSubmissionFee / 1e18,
-//             " USD"
-//         );
-//     }
+        submitEvidence(vSkillUser);
+    }
+}
 
-//     function run() external {
-//         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
-//             "VSkillUser",
-//             block.chainid
-//         );
+contract ChangeSubmissionFee is Script {
+    uint256 newSubmissionFeeInUsd = 20e18; // 20 USD
 
-//         console.log("Most recently deployed address: ", mostRecentlyDeployed);
+    function changeSubmissionFee(address vSkillUser) public {
+        vm.startBroadcast();
 
-//         changeSubmissionFeeVSkillUser(
-//             mostRecentlyDeployed,
-//             NEW_SUBMISSION_FEE_IN_USD
-//         );
-//     }
-// }
+        VSkillUser vSkillUserInstance = VSkillUser(payable(vSkillUser));
+        vSkillUserInstance.changeSubmissionFee(newSubmissionFeeInUsd);
 
-// contract AddMoreSkillsVSkillUser is Script {
-//     string public constant NEW_SKILL_DOMAIN = "New skill domain";
-//     string public constant NEW_NFT_IMAGE_URI = "newnftimageuri";
+        vm.stopBroadcast();
 
-//     function addMoreSkillsVSkillUser(
-//         address mostRecentlyDeployed,
-//         string memory newSkillDomain,
-//         string memory newNftImageUri
-//     ) public {
-//         vm.startBroadcast();
-//         VSkillUser vskill = VSkillUser(payable(mostRecentlyDeployed));
-//         vskill.addMoreSkills(newSkillDomain, newNftImageUri);
-//         vm.stopBroadcast();
+        console.log(
+            "Submission fee changed successfully on chain id: ",
+            block.chainid,
+            " to: ",
+            newSubmissionFeeInUsd
+        );
+    }
 
-//         string[] memory skillDomains = vskill.getSkillDomains();
-//         uint256 length = skillDomains.length;
-//         for (uint256 i = 0; i < length; i++) {
-//             console.log("Skill domain: ", skillDomains[i]);
-//         }
-//     }
+    function run() external {
+        address vSkillUser = Vm(address(vm)).getDeployment(
+            "VSkillUser",
+            uint64(block.chainid)
+        );
 
-//     function run() external {
-//         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
-//             "VSkillUser",
-//             block.chainid
-//         );
+        changeSubmissionFee(vSkillUser);
+    }
+}
 
-//         console.log("Most recently deployed address: ", mostRecentlyDeployed);
+contract WithdrawProfit is Script {
+    function withdrawProfit(address vSkillUser) public {
+        vm.startBroadcast();
 
-//         addMoreSkillsVSkillUser(
-//             mostRecentlyDeployed,
-//             NEW_SKILL_DOMAIN,
-//             NEW_NFT_IMAGE_URI
-//         );
-//     }
-// }
+        VSkillUser vSkillUserInstance = VSkillUser(payable(vSkillUser));
+        vSkillUserInstance.withdrawProfit();
 
-// contract CheckFeedbackOfEvidenceVSkillUser is Script {
-//     function checkFeedbackOfEvidenceVSkillUser(
-//         address mostRecentlyDeployed,
-//         uint256 indexOfUserEvidence
-//     ) public {
-//         vm.startBroadcast();
-//         VSkillUser vskill = VSkillUser(payable(mostRecentlyDeployed));
-//         vskill.checkFeedbackOfEvidence(indexOfUserEvidence);
-//         vm.stopBroadcast();
+        vm.stopBroadcast();
 
-//         StructDefinition.VSkillUserEvidence[] memory evidence = vskill
-//             .getEvidences();
+        console.log(
+            "Profit withdrawn successfully on chain id: ",
+            block.chainid
+        );
+    }
 
-//         string[] memory feedback = evidence[indexOfUserEvidence]
-//             .feedbackIpfsHash;
-//         if (feedback.length == 0) {
-//             console.log("No feedback provided yet");
-//         } else {
-//             uint256 length = feedback.length;
-//             for (uint256 i = 0; i < length; i++) {
-//                 console.log("Feedback IPFS hash: ", feedback[i]);
-//             }
-//         }
-//     }
+    function run() external {
+        address vSkillUser = Vm(address(vm)).getDeployment(
+            "VSkillUser",
+            uint64(block.chainid)
+        );
 
-//     function run() external {
-//         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
-//             "VSkillUser",
-//             block.chainid
-//         );
-
-//         console.log("Most recently deployed address: ", mostRecentlyDeployed);
-
-//         checkFeedbackOfEvidenceVSkillUser(mostRecentlyDeployed, 0);
-//     }
-// }
-
-// contract EarnUserNft is Script {
-//     using StructDefinition for StructDefinition.VSkillUserEvidence;
-
-//     StructDefinition.VSkillUserEvidence public evidence;
-//     string public constant IPFS_HASH =
-//         "https://ipfs.io/ipfs/QmbJLndDmDiwdotu3MtfcjC2hC5tXeAR9EXbNSdUDUDYWa";
-//     string public constant SKILL_DOMAIN = "Blockchain";
-
-//     function earnUserNft(
-//         address mostRecentlyDeployed,
-//         StructDefinition.VSkillUserEvidence memory ev
-//     ) public {
-//         vm.startBroadcast();
-//         VSkillUser vskill = VSkillUser(payable(mostRecentlyDeployed));
-//         vskill.earnUserNft(ev);
-//         vm.stopBroadcast();
-
-//         console.log("User NFT minted successfully");
-//         uint256 tokenCounter = vskill.getTokenCounter();
-
-//         console.log("Token ID: ", tokenCounter - 1);
-//     }
-
-//     function run() external {
-//         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
-//             "VSkillUser",
-//             block.chainid
-//         );
-
-//         console.log("Most recently deployed address: ", mostRecentlyDeployed);
-
-//         // In case to see the minted NFT, here we just provide the evidence which is already approved
-//         // ISSUE: Is that mean anyone can mint NFT by providing the approved evidence?
-//         StructDefinition.VSkillUserEvidence memory ev = StructDefinition
-//             .VSkillUserEvidence(
-//                 msg.sender,
-//                 IPFS_HASH,
-//                 SKILL_DOMAIN,
-//                 StructDefinition.VSkillUserSubmissionStatus.APPROVED,
-//                 new string[](0)
-//             );
-
-//         earnUserNft(mostRecentlyDeployed, ev);
-//     }
-// }
+        withdrawProfit(vSkillUser);
+    }
+}
